0c6c90ab3dc0db7ff16dfbc9d76f5937fb669078
Gaurav Kumar <gauravsyr10@gmail.com>
2020-12-01 09:44:02 +0530

    completed video progress bar and timestamp

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -24,10 +24,28 @@ function updatePlayIcon() {
 }
 
 // Update progress & timestamp
-function updateProgress() {}
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
 
 // Set video time to progress
-function setVideoProgress() {}
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
 
 // Stop video
 function stopVideo() {
